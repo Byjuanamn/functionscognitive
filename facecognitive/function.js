3e5452e99f6b252f02a1406fe553dd90b3ec21b6
byjuanamn <juan.antonio.martin@gmail.com>
2016-07-06 12:37:30 +0200

    add index and function files

---
+++ facecognitive/function.js
@@ -0,0 +1,19 @@
+/**
+ * Created by byjuanmn on 06/07/16.
+ */
+{
+    "bindings": [
+    {
+        "authLevel": "function",
+        "type": "httpTrigger",
+        "direction": "in",
+        "name": "req"
+    },
+    {
+        "type": "http",
+        "direction": "out",
+        "name": "res"
+    }
+],
+    "disabled": false
+}
